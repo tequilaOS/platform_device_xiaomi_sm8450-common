07c34d227777a9111e8867fe6f5bbb743bea649e
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-11-14 22:46:05 +0530

    sm8450-common: Replace to use android.hardware.gnss-V1-ndk.so
    Change-Id: I919f7054b760209a187fc3b6127973f2d2f05af1
    Signed-off-by: Anush02198 <anush.4376@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -68,6 +68,21 @@ function blob_fixup() {
             ;;
         vendor/bin/hw/android.hardware.security.keymint-service-qti)
             ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
+	    ;;
+        vendor/lib64/libgarden.so)
+            "${PATCHELF}" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
+            ;;
+        vendor/lib64/libgarden_haltests_e2e.so)
+            "${PATCHELF}" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
+            ;;
+        vendor/lib/hw/android.hardware.gnss-aidl-impl-qti.so)
+            "${PATCHELF}" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
+            ;;
+        vendor/lib64/hw/android.hardware.gnss-aidl-impl-qti.so)
+            "${PATCHELF}" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
+            ;;
+        vendor/bin/hw/android.hardware.gnss-aidl-service-qti)
+            "${PATCHELF}" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
             ;;
     esac
 }
